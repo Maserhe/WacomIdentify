02fc7a6a4123682dfb1dd482840004c38ebf5386
Maserhe <982289931@qq.com>
2021-09-29 18:47:37 +0800

    更改实验类型

---
+++ identify/src/ViewInfo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * 描述:
+ *
+ * @author Maserhe
+ * @create 2021-05-20 22:21
+ */
+public class ViewInfo {
+
+    private String user;
+    private String path;
+
+    // 简单任务 和复杂 任务 需要的时间。
+    private Integer time;
+
+    // 实验的 次数
+    private Integer count;
+
+    // 读取 数据 显示的 字体。 规定内容
+    public static ArrayList<String> info0;
+
+    // 读取 非规定的内容
+    public static ArrayList<ArrayList<WritePoint>> info1;
+
+
+
+}
